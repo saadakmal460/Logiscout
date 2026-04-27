73656154f75665e9c6fc375ad276571c08164ae5
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2026-04-27 23:05:06 +0500

    API token

--- src/api/LogApi.ts
+++ src/api/LogApi.ts
@@ -2,15 +2,17 @@ import {
   BackendLogPayload,
   FrontendLogPayload,
 } from "../core/interface/payloads/Payloads.js";
-
-import {HTTP} from "./Interceptor.js";
+import { getLogiScouConfig } from "../initiator/state.js";
+import { HTTP } from "./Interceptor.js";
 
 type LogPayload = BackendLogPayload | FrontendLogPayload;
 
 export async function LogApi(data: LogPayload[]): Promise<void> {
   try {
-    // console.log(data)
-    const response = await HTTP.post("/ingest", data);
+    const { apiKey } = getLogiScouConfig();
+    await HTTP.post("/ingest", data, {
+      headers: apiKey ? { Authorization: apiKey } : undefined,
+    });
   } catch (error) {
     console.error("Failed to send log:", error);
   }
